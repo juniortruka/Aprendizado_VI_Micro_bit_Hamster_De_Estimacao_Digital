ceca90d75534a279e7b8a8db0da16de0eefeadcd
Júnior Truká <74542414+juniortruka@users.noreply.github.com>
2025-11-17 16:39:05 -0300

    Create Aprendizado_VI_Micro_bit_Hamster_De_Estimacao_Digital.ts
    v1

---
+++ Aprendizado_VI_Micro_bit_Hamster_De_Estimacao_Digital.ts
@@ -0,0 +1,17 @@
+//Hamster de estimação Digital
+// Ao pressionar a logo da placa
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    // Mostrar o icone Hamster
+    basic.showIcon(IconNames.Asleep)
+    // Tocar uma música/melodia;
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+})
+// Ao agitar/sacudir a placa;
+input.onGesture(Gesture.Shake, function () {
+    // Icone triste.
+    basic.showIcon(IconNames.Sad)
+    // Tocar melodia triste.
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+})
+// Moistrar icone inicial
+basic.showIcon(IconNames.TShirt)
