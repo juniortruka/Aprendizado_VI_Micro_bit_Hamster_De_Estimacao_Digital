8926816932e05ae5199c5cb88eb18059a97fc7a5
Júnior Truká <74542414+juniortruka@users.noreply.github.com>
2025-11-17 20:43:05 -0300

    Update Aprendizado_VI_Micro_bit_Hamster_De_Estimacao_Digital.ts
    v3

--- Aprendizado_VI_Micro_bit_Hamster_De_Estimacao_Digital.ts
+++ Aprendizado_VI_Micro_bit_Hamster_De_Estimacao_Digital.ts
@@ -4,7 +4,7 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     // Mostrar o icone Hamster
     basic.showIcon(IconNames.Asleep)
     // Tocar uma música/melodia;
-    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.InBackground)
 })
 // Ao agitar/sacudir a placa;
 input.onGesture(Gesture.Shake, function () {
@@ -16,4 +16,7 @@ input.onGesture(Gesture.Shake, function () {
 })
 // Moistrar icone inicial
 basic.showIcon(IconNames.TShirt)
+basic.pause(2000)
+basic.clearScreen()
 basic.showIcon(IconNames.Happy)
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
