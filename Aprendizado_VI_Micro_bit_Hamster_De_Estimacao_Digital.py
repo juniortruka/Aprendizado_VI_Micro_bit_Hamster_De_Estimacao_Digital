e250e4e26a38c9e77a8620ea26f55bd4219eb25c
Júnior Truká <74542414+juniortruka@users.noreply.github.com>
2025-11-17 16:39:53 -0300

    Update Aprendizado_VI_Micro_bit_Hamster_De_Estimacao_Digital.py
    v1

--- Aprendizado_VI_Micro_bit_Hamster_De_Estimacao_Digital.py
+++ Aprendizado_VI_Micro_bit_Hamster_De_Estimacao_Digital.py
@@ -1,1 +1,23 @@
+# Hamster de estimação Digital
+# Ao pressionar a logo da placa
 
+def on_logo_pressed():
+    # Mostrar o icone Hamster
+    basic.show_icon(IconNames.ASLEEP)
+    # Tocar uma música/melodia;
+    music._play_default_background(music.built_in_playable_melody(Melodies.ENTERTAINER),
+        music.PlaybackMode.IN_BACKGROUND)
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+# Ao agitar/sacudir a placa;
+
+def on_gesture_shake():
+    # Icone triste.
+    basic.show_icon(IconNames.SAD)
+    # Tocar melodia triste.
+    music._play_default_background(music.built_in_playable_melody(Melodies.DADADADUM),
+        music.PlaybackMode.IN_BACKGROUND)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+# Moistrar icone inicial
+basic.show_icon(IconNames.TSHIRT)
